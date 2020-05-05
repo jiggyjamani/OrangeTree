eaec09635f8f5136cecab53d0a4ff5061ffd621e
Jamani <jamanifowler5@gmail.com>
2020-05-05 16:40:26 -0400

    initial

---
+++ OrangeTree/File.swift
@@ -0,0 +1,9 @@
+//
+//  File.swift
+//  OrangeTree
+//
+//  Created by Jamani Fowler on 1/30/20.
+//  Copyright © 2020 Jamani Fowler. All rights reserved.
+//
+
+import Foundation
---
+++ OrangeTree/Orange.swift
@@ -0,0 +1,23 @@
+//
+//  Orange.swift
+//  OrangeTree
+//
+//  Created by Jamani Fowler on 1/30/20.
+//  Copyright © 2020 Jamani Fowler. All rights reserved.
+//
+
+import SpriteKit
+
+class Orange: SKSpriteNode {
+  init() {
+    let texture = SKTexture(imageNamed: "Orange")
+    let size = texture.size()
+    let color = UIColor.clear
+super.init(texture: texture, color: color, size: size)
+
+physicsBody = SKPhysicsBody(circleOfRadius: size.width / 2)
+}
+
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    } }
